af2429837811fc23de0673494c79f299ecfd4e37
Victor Barca <victorblinares@gmail.com>
2019-02-23 01:23:03 +0000

    test server

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Victor on 22/02/2019.
+ */
+
+import static  spark.Spark.get;
+
+public class Main {
+    public static void main (String[] args){
+        System.out.println("Hello world");
+        get("/test", (request, response) -> "User: username: test, email=test@test.net");
+    }
+}
